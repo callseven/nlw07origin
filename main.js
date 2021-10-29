5f3fb3707412b0103fa4c592ed6002bf19a4f0af
callseven <callseven@me.com>
2021-10-28 22:18:50 -0300

    Final Projeto

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
       const LinksSocialMedia = {
-        github: 'jakeliny',
+        github: 'callseven',
         youtube: 'jakelinygracielly',
         facebook: 'maykbrito',
         instagram: 'jakeliny.gracielly',
@@ -12,23 +12,23 @@
 
           li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
 
-          alert(li.children[0].href)
+          // alert(li.children[0].href)
         }
       }
 
       changeSocialMediaLinks()
 
       function getGitHubProfileInfos() {
-          const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+        const url = `https://api.github.com/users/${LinksSocialMedia.github}`
           
-          fetch(url).then(response => )
-      
+        fetch(url)
         .then(response => response.json())
         .then(data => {
         userName.textContent = data.name
         userBio.textContent = data.bio
+        // userLink.textContent = data.login
         userLink.href = data.html_url
-        UserImage.src = data.avatar_url
+        userImage.src = data.avatar_url
         userLogin.textContent = data.login
         })
     }
@@ -41,3 +41,11 @@ getGitHubProfileInfos()
 
       // ser humano 12345678910
       //computador 0123456789
+      
+      //ARROW FUNCTION
+      function name(params) {
+          //code
+      }
+      params => {} // recebendo um parametro (argumento) ou 
+      (param1, param2) => {} //ou (recebendo mais de um argumento)
+      () => {} // funcao sem receber nada
